88a2a5be34625d9c2ee7caa2e3be55fcc1762de5
Petter Säterskog <petter@Petters-MacBook-Pro.local>
2020-03-25 22:58:09 +0100

    added pickles for some countries

--- covid19.py
+++ covid19.py
@@ -13,12 +13,13 @@ from scipy.special import gammaln
 from scipy.optimize import minimize
 import pickle
 
+country = 'US'
 #cache the results to not unnecessarily tax their server
 if False:
 	import COVID19Py
-	covid19 = COVID19Py.COVID19()
-	country = 'SE'
-	res = covid19.getLocationByCountryCode("SE", timelines=True)
+	covid19 = COVID19Py.COVID19(data_source="jhu") #Johns Hopkins University
+	
+	res = covid19.getLocationByCountryCode(country, timelines=True)
 
 	#categories are: 'cofirmed', 'deaths', 'recovered'
 	deathDict = {k:v for k,v in res[0]['timelines']['deaths']['timeline'].items() if 0<v}
@@ -26,29 +27,29 @@ if False:
 	dates = sorted(deathDict.keys())
 	datesSinceLatest = list(range(1-len(dates),1))
 	deaths=[deathDict[d] for d in dates]
-	pickle.dump( (dates, datesSinceLatest, deaths), open( "cache.p", "wb" ) )
+	pickle.dump( (dates, datesSinceLatest, deaths), open( "cache{}.p".format(country), "wb" ) )
 else:
-	dates, datesSinceLatest, deaths = pickle.load( open( "cache.p", "rb" ) )
+	dates, datesSinceLatest, deaths = pickle.load( open( "cache{}.p".format(country), "rb" ) )
+print("Latest number of deaths in {}: {}".format(country, deaths[-1]))
 
-print("Latest number of deaths in Sweden: " + str(deaths[-1]))
-
-append=66
+#append today's number if it is not yet available in the service
+append=0 #66
 if append:
 	dates.append('today')
 	datesSinceLatest=list(range(1-len(dates),1))
 	deaths.append(append)
-	print (datesSinceLatest)
 
 def likelihood(v, l, dates, deaths):
 	return sum(deaths[i]*(log(v)+dates[i]*l)-v*exp(dates[i]*l) for i in range(len(deaths)))
 	#return sum(deaths[i]*(log(v)+dates[i]*l)-v*exp(dates[i]*l)-gammaln(deaths[i]+1) for i in range(len(deaths)))
 	#return sum(np.log(poisson.pmf(deaths[i], v*np.exp(dates[i]*l))) for i in range(len(deaths)))
 
-res = minimize(lambda x: -likelihood(x[0], x[1], datesSinceLatest, deaths), [1,0], bounds=[(0,np.inf),(0,np.inf)]).x
-print(res)
+eps=1e-2
+res = minimize(lambda x: -likelihood(x[0], x[1], datesSinceLatest, deaths), [1,0], bounds=[(eps,np.inf),(eps,np.inf)]).x
+print("Doubling time: {:.2f} days".format(log(2)/res[1]))
 allDates = np.arange(datesSinceLatest[0], 45)
 pl.plot(allDates, res[0]*exp(allDates*res[1]), label='MLE estimation of exponential growth.')
-pl.plot(datesSinceLatest, deaths, 'x', label='Covid19 deaths in Sweden')
+pl.plot(datesSinceLatest, deaths, 'x', label='Covid19 deaths in '+country)
 pl.yscale('log')
 pl.xlabel('days after '+dates[-1][:10])
 pl.ylabel('deaths')
