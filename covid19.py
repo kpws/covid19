4f39328898f9fb631223921785fd3cc6d6dc46fa
Petter Säterskog <petter@Petters-MacBook-Pro.local>
2020-03-25 23:30:25 +0100

    fixed bug where fitting was done on total deaths instead of deaths by day

--- covid19.py
+++ covid19.py
@@ -13,7 +13,7 @@ from scipy.special import gammaln
 from scipy.optimize import minimize
 import pickle
 
-country = 'US'
+country = 'SE'
 #cache the results to not unnecessarily tax their server
 if False:
 	import COVID19Py
@@ -30,26 +30,30 @@ if False:
 	pickle.dump( (dates, datesSinceLatest, deaths), open( "cache{}.p".format(country), "wb" ) )
 else:
 	dates, datesSinceLatest, deaths = pickle.load( open( "cache{}.p".format(country), "rb" ) )
-print("Latest number of deaths in {}: {}".format(country, deaths[-1]))
 
-#append today's number if it is not yet available in the service
+print('Country: '+country)
+print('Latest number of deaths: {}'.format(deaths[-1]))
+
+#append today's number if it is not yet available on the server
 append=0 #66
 if append:
 	dates.append('today')
 	datesSinceLatest=list(range(1-len(dates),1))
 	deaths.append(append)
 
-def likelihood(v, l, dates, deaths):
-	return sum(deaths[i]*(log(v)+dates[i]*l)-v*exp(dates[i]*l) for i in range(len(deaths)))
+def likelihood(v, l, dates, deathsPerDay):
+	return sum(deathsPerDay[i]*(log(v)+dates[i]*l)-v*exp(dates[i]*l) for i in range(len(deathsPerDay)))  #removed constant terms
 	#return sum(deaths[i]*(log(v)+dates[i]*l)-v*exp(dates[i]*l)-gammaln(deaths[i]+1) for i in range(len(deaths)))
-	#return sum(np.log(poisson.pmf(deaths[i], v*np.exp(dates[i]*l))) for i in range(len(deaths)))
+	#return sum(np.log(poisson.pmf(deaths[i], v*np.exp(dates[i]*l))) for i in range(len(deaths))) #same but not numerically stable
 
-eps=1e-2
-res = minimize(lambda x: -likelihood(x[0], x[1], datesSinceLatest, deaths), [1,0], bounds=[(eps,np.inf),(eps,np.inf)]).x
+deathsPerDay=np.diff(deaths, prepend=0)
+res = minimize(lambda x: -likelihood(x[0], x[1], datesSinceLatest, deathsPerDay), [1,0], bounds=[(0,np.inf),(0,np.inf)]).x
 print("Doubling time: {:.2f} days".format(log(2)/res[1]))
 allDates = np.arange(datesSinceLatest[0], 45)
-pl.plot(allDates, res[0]*exp(allDates*res[1]), label='MLE estimation of exponential growth.')
-pl.plot(datesSinceLatest, deaths, 'x', label='Covid19 deaths in '+country)
+pl.plot(allDates, res[0]*exp(allDates*res[1])/res[1], label='Exponential growth, total deaths.')
+pl.plot(allDates, res[0]*exp(allDates*res[1]), label='Exponential growth, deaths per day.')
+pl.plot(datesSinceLatest, deaths, 'x', label='Covid19 total deaths in '+country)
+pl.plot(datesSinceLatest, deathsPerDay, 'x', label='Covid19 deaths per day in '+country)
 pl.yscale('log')
 pl.xlabel('days after '+dates[-1][:10])
 pl.ylabel('deaths')
